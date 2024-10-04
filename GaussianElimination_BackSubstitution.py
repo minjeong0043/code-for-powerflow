ff0470f3c329ed228bdce7206b8e8eed4d0df7a9
minjeong0043 <129171603+minjeong0043@users.noreply.github.com>
2024-10-04 17:03:22 +0900

    Add files via upload

---
+++ GaussianElimination_BackSubstitution.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def GaussianElimination(A, y):
+    size_mtx = len(A)
+    for i in range(size_mtx):
+        for j in range(i+1, size_mtx):
+            k = A[j][i]/A[i][i]
+            A[j] = np.subtract(A[j],np.multiply(k, A[i]))
+            y[j] = y[j] - y[i]*k
+    return A,y
+
+def BackSubstitution(A, y):
+    size_mtx = len(A)
+    x = np.zeros(size_mtx)
+    x[size_mtx-1] = 1/A[size_mtx-1][size_mtx-1]*y[size_mtx-1]
+    p = 0
+    for i in range(size_mtx-2,-1,-1):
+        for j in range(i+1, size_mtx):
+            p += A[i][j]*x[j]
+        x[i] = 1/A[i][i] * (y[i] - p)
+    return x
+
+#A = np.array([[10,5],[2,9]],dtype=float)
+#y = np.array([6,3],dtype=float)
+A = np.array([[2, 3, -1], [-4, 6, 8], [10,12,14]], dtype=float)
+y = np.array([5, 7, 9], dtype=float)
+
+A, y = GaussianElimination(A,y);
+x = BackSubstitution(A, y);
+print("A", A)
+print("y", y)
+print("x", x)
