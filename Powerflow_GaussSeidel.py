3615f3b829f5fa7de5f19bdf4fd42958b8490721
minjeong0043 <129171603+minjeong0043@users.noreply.github.com>
2024-10-16 10:49:05 +0900

    Add files via upload

--- Powerflow_GaussSeidel.py
+++ Powerflow_GaussSeidel.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
-from sympy.printing.precedence import precedence_Integer
+# from sympy.printing.precedence import precedence_Integer
+from Powerflow_func import Ybus, GaussSeidel_PQ
 
 # read input file and define parameters
-data_path = "ac_case25.xlsx"
+data_path = "../../Downloads/ac_case25.xlsx"
 
 bus = pd.read_excel(data_path, sheet_name='bus')
 branch = pd.read_excel(data_path, sheet_name='branch')
@@ -19,44 +20,31 @@ Pload_pu = Pload/Sbase
 Qload_pu = Qload/Sbase
 
 # Calculate Ybus
-From = branch['From']
-To = branch['To']
-R = branch['R (pu)']
-X = branch['X (pu)']
-B = branch['B (pu)']
 size_Mtx = len(bus)
-
-Y = np.zeros([size_Mtx, size_Mtx], dtype = complex)
-
-for i in range(size_Mtx):
-    for j in range(size_Mtx):
-        if i == j:
-            index = np.where(From == i)[0]
-            if index.size == 0:
-                Y[i,j] = 0
-            else:
-                for k in index:
-                    Y[i,j] += 1 / (R[k] + 1j*X[k]) + 1j*B[k]/2
-        else:
-            index = np.where(((From == i) & (To == j)) | ((From == j) & (To == i)))[0]
-            if index.size == 0:
-                Y[i,j] = 0
-            else:
-                # Y[i,j] = -1/(R[index] + 1j*X[index])
-                for k in index:
-                    Y[i,j] -= 1/(R[k] + 1j*X[k])
-
+Y = Ybus(branch, size_Mtx)
 
 # Gauss Seidel method according to bus Type
 Type = bus['Type']
-# print(Type)
+# for i in range(len(Type)):
+#     if Type[i] == 'PV':
+#         print("PV bus")
+#         GaussSeidel_PV()
+#     elif Type[i] == 'PQ':
+#         print("PQ bus")
+#         GaussSeidel_PQ()
+#     elif Type[i] == 'Swing':
+#         print("Swing bus")
+#         GaussSeidel_Swing()
+
+# TEST GaussSeidel_PQ()
+V = np.ones(size_Mtx, dtype=complex)
 for i in range(len(Type)):
-    if Type[i] == 'PV':
-        print("PV bus")
-        GaussSeidel_PV()
-    elif Type[i] == 'PQ':
-        print("PQ bus")
-        GaussSeidel_PQ()
-    elif Type[i] == 'Swing':
-        print("Swing bus")
-        GaussSeidel_Swing()
+    if Type[i] == 'PQ':
+        print("i = ", i)
+        V = GaussSeidel_PQ(Pload_pu, Qload_pu, Y, i, V)
+        print("V : ", V)
+
+
+
+
+
--- Powerflow_func.py
+++ Powerflow_func.py
@@ -1,6 +1,52 @@
+import numpy as np
 
-def GaussSeidel_PQ():
+def GaussSeidel_PQ(P, Q, Y, i, V): #load bus
+    size_Mtx = len(P)
+    # V = np.ones(size_Mtx, dtype = complex)
+    b = np.zeros(1, dtype=complex)
+    for s in range(size_Mtx):
+        if s!= i:
+            # print("s",s)
+            b += Y[i][s] *V[s]
+            # print("Y : ", Y[i][s])
+            # print('V : ', V[s])
+            # print(b)
+    # iteration 1
+    V[i] = (1 / Y[i][i]) * (((P[i] - 1j*Q[i]) / V[i].conjugate()) - b)
+    # iteration 2
+    V[i] = (1 / Y[i][i]) * (((P[i] - 1j*Q[i]) / V[i].conjugate()) - b)
 
-def GaussSeidel_PV():
+    return V
 
-def GaussSeidel_Swing():
+
+# def GaussSeidel_PV():
+#
+# def GaussSeidel_Swing():
+
+def Ybus(branch, size_Mtx):
+    From = branch['From'] -1
+    To = branch['To'] -1
+    R = branch['R (pu)']
+    X = branch['X (pu)']
+    B = branch['B (pu)']
+
+    Y = np.zeros([size_Mtx, size_Mtx], dtype= complex)
+
+    for i in range(size_Mtx):
+        for j in range(size_Mtx):
+            if i == j:
+                index = np.where(From ==i)[0]
+                if index.size== 0:
+                    Y[i,j] = 0
+                else:
+                    for k in index:
+                        Y[i,j] += 1/(R[k] + 1j*X[k]) + 1j*B[k]/2
+            else:
+                index = np.where(((From == i) & (To == j)) | ((From == j) & (To == i)))[0]
+                if index.size == 0:
+                    Y[i,j] = 0
+                else:
+                    for k in index:
+                        Y[i,j] -= 1/(R[k] + 1j*X[k])
+
+    return Y
