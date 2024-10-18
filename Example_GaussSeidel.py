8fd9db62c7e20b5a100ce32209dfdbcadc206e95
minjeong0043 <129171603+minjeong0043@users.noreply.github.com>
2024-10-18 17:56:07 +0900

    Add files via upload

--- Example_GaussSeidel.py
+++ Example_GaussSeidel.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
-from Example_Powerflow_func import Ybus
+from Example_Powerflow_func import Ybus, Cal_PQ, GaussSeidel_PQ
 
+# Read Data
 data_name = "ac_case25_example.xlsx"
 bus = pd.read_excel(data_name, sheet_name='bus')
 branch = pd.read_excel(data_name, sheet_name='branch')
@@ -10,18 +11,72 @@ branch = pd.read_excel(data_name, sheet_name='branch')
 size_bus = len(bus)
 Y = Ybus(branch, size_bus)
 
-# 각 버스의 전압 크기와 위상 초기화
-V = np.ones(size_bus) # |V|
+# Initial voltage assignments for all buses
+V_value = np.ones(size_bus) # |V|
 delta = np.zeros(size_bus) # 위상
+V = np.ones(size_bus, dtype=complex)
 
 Swing_bus_index = bus[bus['Type'] == 'Swing'].index[0]
-pv_buses = bus[bus['Type'] == 'PV'].index
+PV_buses = bus[bus['Type'] == 'PV'].index[0]
+V_value[Swing_bus_index] = bus['V (pu)'][Swing_bus_index]
+V_value[PV_buses] = bus['V (pu)'][PV_buses]
 
-V[]
-# PQ bus
-# bus 2
-P2_cal = 0
-Q2_cal = 0
-t = 0
-for i in range(size_bus):
-    V
+V = V_value * np.exp(1j*delta)
+
+# 버스에 따라 각 가우스자이델 반복법 적용!
+# index_without_Swing = np.where(bus['Type'] != 'Swing')[0]
+
+# for bus_index in index_without_Swing:
+#     # print(bus['Type'][bus_index])
+#     if bus['Type'][bus_index] == 'PQ':
+#         print("bus_index : ",bus_index)
+#         V = GaussSeidel_PQ(bus, bus_index, V, Y)
+#         print(f"V : {V}")
+#     elif bus['Type'][bus_index] == 'PV':
+#         print("PV")
+
+# PV bus
+
+tolerance =1e-6
+max_iter = 100
+iteration = 0
+converged = False
+
+index_PV = np.where(bus['Type'] == "PV")[0][0]
+print("Type(index_PV) : ", type(index_PV))
+print(index_PV)
+
+# del_p와 del_V_value
+P_given = bus['PG (pu)'] - bus['PL (pu)']
+V_given = bus['V (pu)'][index_PV]
+
+# P_cal, _ = Cal_PQ(V, Y, index_PV, len(bus))
+# V_cal =  # 고려 없이 계산해보고 비교하기
+# del_P = abs(P_given[index_PV] - P_cal)
+# del_V = abs(V_given - V_cal)
+# print(len(V_given))
+# print(V_given-1)
+Q = 0 # PV모선에서의 변수
+V_given = V[index_PV]
+while not converged and iteration < max_iter:
+    P_cal, _ = Cal_PQ(V, Y, index_PV, len(bus))
+    print("V_given : ", V_given)
+    del_P = abs(P_given[index_PV] - P_cal)
+    # np.power(del_V, 2) = np.power(V_given, 2) - np.power(abs(V[index_PV]), 2) # del_V 는 크기임. 위상 x, V_given은 크기로 주어졌고 ,,,
+    del_V = (np.power(V_given, 2) - np.power(abs(V[index_PV], 2)))**0.5
+    # del_P = abs(V)
+    print("V[index_PV]                          : ",V[index_PV])
+
+    if ((del_P < tolerance) and (del_V < tolerance)):
+        converged = True
+        print(f"Converged at i = {iteration}, V = {V[index_PV]}")
+        break
+
+    b = 0
+    for j in range(len(bus)):
+        if index_PV != j:
+            b += Y[index_PV, j] * V[j]
+    V[index_PV] = (1/Y[index_PV, index_PV]) * (((P_given[index_PV] - 1j* Q) / V[index_PV].conjugate()) - b)
+    print(del_P)
+    iteration += 1
+
--- Example_Powerflow_func.py
+++ Example_Powerflow_func.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import cmath
 
 def Ybus(branch, size_Mtx):
     Y = np.zeros([size_Mtx, size_Mtx], dtype= complex)
@@ -22,3 +23,62 @@ def Ybus(branch, size_Mtx):
                         Y[i,j] -= 1/(branch['R (pu)'][k] + 1j*branch['X (pu)'][k])
 
     return Y
+
+# def GaussSeidel_PQ():
+#     while True:
+#         del =
+
+def Cal_PQ(V, Y, i, size_bus): # 전압 크기, 위상, 어드미턴스, bus i에서의 계산 PQ 전력
+    # V_value, delta = cmath.polar(V)
+    V_value = []
+    delta = []
+
+    # V 배열의 각 요소에 대해 크기와 각도 계산
+    for v in V:
+        v_val, v_delta = cmath.polar(v)
+        V_value.append(v_val)
+        delta.append(v_delta)
+
+    # print("delta[i]", delta[i])
+
+    P_cal = 0
+    Q_cal = 0
+    # t = 0
+    for j in range(size_bus):
+        P_cal += V_value[i] * V_value[j] * (Y[i][j].real * np.cos(delta[i] - delta[j]) + Y[i][j].imag * np.sin(delta[i] - delta[j]))
+        Q_cal += V_value[i] * V_value[j] * (Y[i][j].real * np.sin(delta[i] - delta[j]) - Y[i][j].imag * np.cos(delta[i] - delta[j]))
+
+    # print("P_cal : ", P_cal)
+    # print("Q_cal : ", Q_cal)
+    return P_cal, Q_cal
+
+def GaussSeidel_PQ(bus, bus_index, V, Y):
+    P_given = bus['PG (pu)'] - bus['PL (pu)']
+    Q_given = bus['QG (pu)'] - bus['QL (pu)']
+
+    tolerance = 1e-6
+    max_iter = 100
+    iteration = 0
+    converged = False
+
+    while not converged and iteration < max_iter:
+        P_cal, Q_cal = Cal_PQ(V, Y, bus_index, len(bus))
+        del_P = abs(P_given[bus_index] - P_cal)
+        del_Q = abs(Q_given[bus_index] - Q_cal)
+        # print(f"del_P : {del_P},                          del_Q : {del_Q},                 V : {V[bus_index]}")
+        if ((del_P < tolerance) and (del_Q < tolerance)):
+            converged = True
+            print(f"Converged at i = {iteration}, V = {V[bus_index]}")
+            break
+
+        b = 0
+        for j in range(len(bus)):
+            if bus_index != j:
+                b += Y[bus_index,j] * V[j]
+        V[bus_index] = (1/Y[bus_index, bus_index]) * (((P_given[bus_index] - 1j* Q_given[bus_index]) / V[bus_index].conjugate()) - b)
+
+        iteration += 1
+        # print(f"iteration = {iteration}")
+
+    return V
+
