9ab11c90b2ced4a14d573f9ffdb202eeb22936e8
minjeong0043 <129171603+minjeong0043@users.noreply.github.com>
2024-10-21 18:27:43 +0900

    Add files via upload

--- Powerflow_GaussSeidel.py
+++ Powerflow_GaussSeidel.py
@@ -1,50 +1,34 @@
 import pandas as pd
 import numpy as np
-# from sympy.printing.precedence import precedence_Integer
-from Powerflow_func import Ybus, GaussSeidel_PQ
+from Powerflow_func import Ybus_considering_trans, GaussSeidel_PQ, GaussSeidel_PV, ReadData_Unit_pu
 
 # read input file and define parameters
-data_path = "../../Downloads/ac_case25.xlsx"
+data_path = "C:\\Users\\leejunwoo\\PycharmProjects\\pythonProject\\ac_case25.xlsx"
 
-bus = pd.read_excel(data_path, sheet_name='bus')
-branch = pd.read_excel(data_path, sheet_name='branch')
-transformer = pd.read_excel(data_path, sheet_name='transformer')
-generator = pd.read_excel(data_path, sheet_name='generator')
-Sbase = pd.read_excel(data_path, sheet_name='param', header=None).iloc[0,1]
+bus_pu, branch, transformer, generator_pu, Sbase = ReadData_Unit_pu(data_path)
 
-Pload = bus['Pload (MW)']
-Qload = bus['Qload (MVAR)']
-
-# Transform from MW, MVAR to pu
-Pload_pu = Pload/Sbase
-Qload_pu = Qload/Sbase
-
-# Calculate Ybus
-size_Mtx = len(bus)
-Y = Ybus(branch, size_Mtx)
-
-# Gauss Seidel method according to bus Type
-Type = bus['Type']
-# for i in range(len(Type)):
-#     if Type[i] == 'PV':
-#         print("PV bus")
-#         GaussSeidel_PV()
-#     elif Type[i] == 'PQ':
-#         print("PQ bus")
-#         GaussSeidel_PQ()
-#     elif Type[i] == 'Swing':
-#         print("Swing bus")
-#         GaussSeidel_Swing()
-
-# TEST GaussSeidel_PQ()
-V = np.ones(size_Mtx, dtype=complex)
-for i in range(len(Type)):
-    if Type[i] == 'PQ':
-        print("i = ", i)
-        V = GaussSeidel_PQ(Pload_pu, Qload_pu, Y, i, V)
-        print("V : ", V)
+Y = Ybus_considering_trans(bus_pu, branch, transformer)
 
 
+# initial voltage assignments for all buses
+V_value = bus_pu['Vm (pu)'].copy()
+V_angle = bus_pu['Va (degree)'].copy()
 
+# PV버스의 경우엔 SETPOINT로 전압을 바꿔줘야함.bus k의 setpoint
+for i in range(len(generator_pu)):
+    V_value.loc[generator_pu['Bus'][i] -1] = generator_pu['Voltage setpoint (pu)'][i]
+V = V_value * np.exp(1j * V_angle)
+
+print(V_value)
+# Gauss Seidel
+index_without_Swing = np.where(bus_pu['Type'] != 'Swing')[0]
+# print(index_without_Swing)
+for bus_index in index_without_Swing:
+    if bus_pu['Type'][bus_index] == 'PQ':
+        V = GaussSeidel_PQ(data_path, bus_index, V, Y)
+        print(f"bus_index : {bus_index} ({bus_pu['Type'][bus_index]}),     V : {V}")
+    elif bus_pu['Type'][bus_index] == 'PV':
+        V = GaussSeidel_PV(data_path, bus_index, V, Y)
+        print(f"bus_index : {bus_index} ({bus_pu['Type'][bus_index]}),     V : {V}")
 
 
--- Powerflow_func.py
+++ Powerflow_func.py
@@ -1,33 +1,32 @@
 import numpy as np
+import pandas as pd
+
+def ReadData_Unit_pu(data_path):
+    # read input file and define parameters
+    bus = pd.read_excel(data_path, sheet_name='bus')
+    branch = pd.read_excel(data_path, sheet_name='branch')
+    transformer = pd.read_excel(data_path, sheet_name='transformer')
+    generator = pd.read_excel(data_path, sheet_name='generator')
+    Sbase = pd.read_excel(data_path, sheet_name='param', header=None).iloc[0, 1]
+
+    # Transform Unit to pu in bus sheet
+    bus['Pload (MW)'] = bus['Pload (MW)'] / Sbase
+    bus['Qload (MVAR)'] = bus['Qload (MVAR)'] / Sbase
+    bus_pu = bus.rename(columns={'Pload (MW)': 'Pload (pu)', 'Qload (MVAR)': 'Qload (pu)'})
+    # Transform Unit to pu in generator sheet
+    generator['PG (MW)'] = generator['PG (MW)'] / generator['MBASE (MW)']
+    generator['QG (MVAR)'] = generator['QG (MVAR)'] / generator['MBASE (MW)']
+    generator['QMAX (MVAR)'] = generator['QMAX (MVAR)'] / generator['MBASE (MW)']
+    generator['QMIN (MVAR)'] = generator['QMIN (MVAR)'] / generator['MBASE (MW)']
+    generator_pu = generator.rename(columns={'PG (MW)': 'PG (pu)', 'QG (MVAR)': 'QG (pu)', 'QMAX (MVAR)': 'QMAX (pu)', 'QMIN (MVAR)': 'QMIN (pu)'})
+
+    return bus_pu, branch, transformer, generator_pu, Sbase
+
 def TransformUnit(bus, Sbase):
     bus['Pload (MW)'] = bus['Pload (MW)'] / Sbase
     bus['Qload (MVAR)'] = bus['Qload (MVAR)'] / Sbase
-    bus.rename(columns = {'Pload (MW)' : 'Pload (pu)', 'Qload (MVAR)' : 'Qlaod (pu)'})
-
-    return bus
-
-def GaussSeidel_PQ(P, Q, Y, i, V): #load bus
-    size_Mtx = len(P)
-    # V = np.ones(size_Mtx, dtype = complex)
-    b = np.zeros(1, dtype=complex)
-    for s in range(size_Mtx):
-        if s!= i:
-            # print("s",s)
-            b += Y[i][s] *V[s]
-            # print("Y : ", Y[i][s])
-            # print('V : ', V[s])
-            # print(b)
-    # iteration 1
-    V[i] = (1 / Y[i][i]) * (((P[i] - 1j*Q[i]) / V[i].conjugate()) - b)
-    # iteration 2
-    V[i] = (1 / Y[i][i]) * (((P[i] - 1j*Q[i]) / V[i].conjugate()) - b)
-
-    return V
-
-
-# def GaussSeidel_PV():
-#
-# def GaussSeidel_Swing():
+    bus_pu = bus.rename(columns = {'Pload (MW)' : 'Pload (pu)', 'Qload (MVAR)' : 'Qlaod (pu)'})
+    return bus_pu
 
 def Ybus(branch, size_Mtx):
     From = branch['From'] -1
@@ -56,3 +55,170 @@ def Ybus(branch, size_Mtx):
                         Y[i,j] -= 1/(R[k] + 1j*X[k])
 
     return Y
+
+def Ybus_considering_trans(bus, branch, transformer):
+    size_Mtx = len(bus)
+    Y = np.zeros([size_Mtx, size_Mtx], dtype=complex)
+
+    trans = []
+    index_trans = []
+    tap = np.zeros(len(branch))
+    for i in range(len(transformer)):
+        trans.append([transformer['From'][i], transformer['To'][i], transformer['Tap'][i]])
+
+    # 변압기 부분 branch에서의 index와 tap 재배치
+    for i in range(len(transformer)):
+        a = np.where((branch['From'] == trans[i][0]) & (branch['To'] == trans[i][1]))[0]
+        b = np.where((branch['From'] == trans[i][1]) & (branch['To'] == trans[i][0]))[0]
+        s = set(a) | set(b)
+        for k in s:
+            tap[k] = trans[i][2]
+        if len(s) != 0:
+            index_trans.append(s)
+    index_trans = [next(iter(s)) for s in index_trans] # set to list
+
+    #어드미턴스 계싼
+    for i in range(size_Mtx):
+        for j in range(size_Mtx):
+            if i == j:
+                index = np.where((branch['From'] == i+1) | (branch['To'] == i+1))[0]
+                index_without_trans = set(index) - set(index_trans)
+                index_with_trans = set(index) & set(index_trans)
+
+                if len(index_without_trans) == 0:
+                    Y[i,j] += 0
+                else:
+                    for k in index_without_trans:
+                        Y[i,j] += 1/(branch['R (pu)'][k] + 1j*branch['X (pu)'][k]) + 1j*branch['B (pu)'][k]/2
+
+                if len(index_with_trans) == 0:
+                    Y[i,j] += 0
+                else:
+                    for k in index_with_trans:
+                        Y[i,j] += (1/(branch['R (pu)'][k] + 1j*branch['X (pu)'][k])) / np.power(tap[k], 2)
+            elif i != j:
+                index = np.where(((branch['From'] == i + 1) & (branch['To'] == j + 1)) | ((branch['From'] == j + 1) & (branch['To'] == i + 1)))[0]
+                index_without_trans = set(index) - set(index_trans)
+                index_with_trans = set(index) & set(index_trans)
+                # if len(index_with_trans) > 1 or len(index_without_trans) > 1: # 두 번씩 나타나는 애들
+                #     print(f"i, j = {i, j},            index_without_trans = {index_without_trans},                   index_with_trans = {index_with_trans}")
+                if (len(index_without_trans) != 0) and (len(index_with_trans) == 0):
+                    Y[i, j] = - 1 / (branch['R (pu)'][next(iter(index_without_trans))] + 1j * branch['X (pu)'][
+                        next(iter(index_without_trans))])
+                elif (len(index_with_trans) != 0) and (len(index_without_trans) == 0):
+                    Y[i, j] = - (1 / (branch['R (pu)'][next(iter(index_with_trans))] + 1j * branch['X (pu)'][
+                        next(iter(index_with_trans))])) / tap[next(iter(index_with_trans))]
+                    # print(f"i, j = {i, j},            index_without_trans = {index_without_trans},                   index_with_trans = {index_with_trans}")
+                elif (len(index_with_trans) == 0) and (len(index_without_trans) == 0):
+                    # print(f"i, j = {i, j},            index_without_trans = {index_without_trans},                   index_with_trans = {index_with_trans}")
+                    Y[i, j] = 0
+                else:
+                    print("WWWWWWWWWWWWWOOOOOOOOOOOOOOOOOWWWWWWWW")
+
+    return Y
+
+def Cal_PQ(V, Y, i, size_bus): # 전압 크기, 위상, 어드미턴스, bus i에서의 계산 PQ 전력
+    # V_value, delta = cmath.polar(V)
+    V_value = []
+    delta = []
+
+    # V 배열의 각 요소에 대해 크기와 각도 계산
+    # for v in V:
+    #     v_val, v_delta = cmath.polar(v)
+    #     V_value.append(v_val)
+    #     delta.append(v_delta)
+
+    V_value = abs(V)
+    delta = np.angle(V)
+
+    P_cal = 0
+    Q_cal = 0
+    # t = 0
+    for j in range(size_bus):
+        P_cal += V_value[i] * V_value[j] * (Y[i][j].real * np.cos(delta[i] - delta[j]) + Y[i][j].imag * np.sin(delta[i] - delta[j]))
+        Q_cal += V_value[i] * V_value[j] * (Y[i][j].real * np.sin(delta[i] - delta[j]) - Y[i][j].imag * np.cos(delta[i] - delta[j]))
+
+    # print("P_cal : ", P_cal)
+    # print("Q_cal : ", Q_cal)
+    return P_cal, Q_cal
+
+def GaussSeidel_PQ(data_path, bus_index, V, Y):
+    bus_pu, branch, transformer, generator_pu, Sbase = ReadData_Unit_pu(data_path)
+    PG = np.zeros(len(bus_pu))
+    QG = np.zeros(len(bus_pu))
+    for i in range(len(generator_pu)):
+        # print(f"generator_pu['Bus'][i] : {generator_pu['Bus'][i]}")
+        PG[generator_pu['Bus'][i] - 1] += generator_pu['PG (pu)'][i]
+        QG[generator_pu['Bus'][i] - 1] += generator_pu['QG (pu)'][i]
+
+    P_given = PG - bus_pu['Pload (pu)']
+    Q_given = QG - bus_pu['Qload (pu)']
+
+    tolerance = 1e-6
+    max_iter = 100
+    iteration = 0
+    converged = False
+
+    while not converged and iteration < max_iter:
+        P_cal, Q_cal = Cal_PQ(V, Y, bus_index, len(bus_pu))
+        del_P = abs(P_given[bus_index] - P_cal)
+        del_Q = abs(Q_given[bus_index] - Q_cal)
+        # print(f"del_P : {del_P},                          del_Q : {del_Q},                 V : {V[bus_index]}")
+        if ((del_P < tolerance) and (del_Q < tolerance)):
+            converged = True
+            print(f"Converged at i = {iteration}, V = {V[bus_index]}")
+            break
+
+        b = 0
+        for j in range(len(bus_pu)):
+            if bus_index != j:
+                b += Y[bus_index,j] * V[j]
+        V[bus_index] = (1/Y[bus_index, bus_index]) * (((P_given[bus_index] - 1j* Q_given[bus_index]) / V[bus_index].conjugate()) - b)
+
+        iteration += 1
+        # print(f"iteration = {iteration}")
+
+    return V
+
+def GaussSeidel_PV(data_path, bus_index, V, Y):
+    bus_pu, branch, transformer, generator_pu, Sbase = ReadData_Unit_pu(data_path)
+    PG = np.zeros(len(bus_pu))
+    QG = np.zeros(len(bus_pu))
+    for i in range(len(generator_pu)):
+        # print(f"generator_pu['Bus'][i] : {generator_pu['Bus'][i]}")
+        PG[generator_pu['Bus'][i] - 1] += generator_pu['PG (pu)'][i]
+        QG[generator_pu['Bus'][i] - 1] += generator_pu['QG (pu)'][i]
+
+    P_given = PG - bus_pu['Pload (pu)']
+    Q = 0
+    V_given = abs(V)
+
+    tolerance = 1e-6
+    max_iter = 100
+    iteration = 0
+    converged = False
+
+    while not converged and iteration < max_iter:
+        P_cal, Q_cal = Cal_PQ(V, Y, bus_index, len(bus_pu))
+        del_P = abs(P_given[bus_index] - P_cal)
+        del_V = abs(abs(V_given) - abs(V[bus_index]))
+        Q = Q_cal
+        # print(f"del_P : {del_P},                          del_V : {del_V},                 V : {V[bus_index]}")
+        if ((del_P < tolerance) and (del_V < tolerance)):
+            converged = True
+            print(f"Converged at i = {iteration}, V = {V[bus_index]}")
+            break
+
+        b = 0
+        for j in range(len(bus_pu)):
+            if bus_index != j:
+                b += Y[bus_index,j] * V[j]
+        V_new = (1/Y[bus_index, bus_index]) * (((P_given[bus_index] - 1j* Q) / V[bus_index].conjugate()) - b)
+        #위 식 다른 거로도 돌려보고 결과 비교해보기
+        V[bus_index] = V_given * np.exp(1j*np.angle(V_new))
+        iteration += 1
+        # print(f"iteration = {iteration}")
+
+    return V
+
+
